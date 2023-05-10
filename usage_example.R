624cf936f575b189552d57f3ca6ce8b81fb034cb
Humberto L. Varona <humberto.varona@yandex.com>
2023-05-10 16:37:08 -0300

    Update usage_example.R

--- usage_example.R
+++ usage_example.R
@@ -1,5 +1,6 @@
-data <- read_data("./test.dat")
+# Example:
 
+data <- read_data("./test.dat")
 plot_data(data, main_title="Sample data", xlab="Category", ylab="Score", ymin=5, ymax=25)
 
-save_plot("./data_plot", "jpeg", 4, 3, 500)
+save_plot("./test", "pdf", 4, 3)
