dabe140cce8b055f11c93aeea1c674893a351887
Humberto L. Varona <humberto.varona@yandex.com>
2023-05-10 16:31:32 -0300

    Update usage_example.R

--- usage_example.R
+++ usage_example.R
@@ -1,5 +1,5 @@
-data <- read_data("/Users/varona/Laboro/Papers/Algorithms_and_apps/R/bargraph/test.dat")
+data <- read_data("./test.dat")
 
 plot_data(data, main_title="Sample data", xlab="Category", ylab="Score", ymin=5, ymax=25)
 
-save_plot("/Users/varona/Laboro/Papers/Algorithms_and_apps/R/bargraph/data_plot", "jpeg", 4, 3, 500)
+save_plot("./data_plot", "jpeg", 4, 3, 500)
